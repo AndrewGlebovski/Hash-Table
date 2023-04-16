49a3cef7e88b379e68b498ca3b6e1d24dd072e3c
Glebovski <andrewglebovski@gmail.com>
2023-04-16 10:21:49 +0300

    Measure hash functions efficiency
    Results are stored in .csv file. Also include .txt and .csv in gitignore

--- source/main.cpp
+++ source/main.cpp
@@ -6,6 +6,13 @@
 #include "text_parser.hpp"
 
 
+/// Contains hash function pointer and it's name
+typedef struct {
+    hash_func_t func = nullptr;     ///< Hash function pointer
+    const char *name = nullptr;     ///< Hash function name
+} HashFuncInfo;
+
+
 /// Hash table possible hash values
 const size_t TABLE_BUFFER_SIZE = 17u;
 
@@ -28,20 +35,43 @@ int main(int argc, char *argv[]) {
     FileInfo info = {};
     mmap_file(argv[1], &info);
 
+    HashFuncInfo func_list[] = {
+        {&hash_1, "return 1"},
+        {&hash_char0, "first char"},
+        {&hash_len, "len"},
+        {&hash_charsum, "char sum"},
+        {&hash_rol, "cyclic left"},
+        {&hash_ror, "cyclic right"},
+        {&gnu_hash, "gnu hash"}
+    };
+
+    FILE *csv_file = fopen("result.csv", "w");
+
+    fprintf(csv_file, "hash function \\ hash value");
+
+    for (size_t i = 0; i < TABLE_BUFFER_SIZE; i++)
+        fprintf(csv_file, ", %lu", i);
+
+    fputc('\n', csv_file);
+
     HashTable table = {};
 
-    hash_table_constructor(&table, TABLE_BUFFER_SIZE, gnu_hash, &cmp_keys, &print_key, &print_data);
+    for (size_t func = 0; func < sizeof(func_list) / sizeof(HashFuncInfo); func++) {
+        hash_table_constructor(&table, TABLE_BUFFER_SIZE, func_list[func].func, &cmp_keys, &print_key, &print_data);
 
-    get_words(&info, &table);
+        get_words(&info, &table);
 
-    fprintf(stdout, "gnu hash: ");
+        fprintf(csv_file, "%s", func_list[func].name);
 
-    for (size_t i = 0; i < table.buffer_size; i++)
-        fprintf(stdout, "%lu ", get_list_len(table.buffer + i));
+        for (size_t i = 0; i < table.buffer_size; i++)
+            fprintf(csv_file, ", %lu", get_list_len(table.buffer + i));
 
-    fputc('\n', stdout);
+        fputc('\n', csv_file);
 
-    hash_table_destructor(&table);
+        hash_table_destructor(&table);
+    }
+
+    fclose(csv_file);
 
     unmap_file(&info);
 
